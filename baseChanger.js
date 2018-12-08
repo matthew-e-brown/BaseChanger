bf0e03ca104e909ced1c592cc84558719f84a1a7
Matthew Brown <matthew.e.brown.17@gmail.com>
2018-12-08 13:24:57 -0500

    Added Regex to remove spaces

--- baseChanger.js
+++ baseChanger.js
@@ -23,7 +23,7 @@ function baseChange(number, fromBase, toBase) {
   const DIGITS_SIXTY_FOUR = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-"];
 
 
-  numString = (typeof number == "string") ? number.split("").reverse() : number.toString().split("").reverse();
+  numString = (typeof number == "string") ? number.replace(/ /g, '').split("").reverse() : number.toString().split("").reverse();
   let decimal = 0;
   for (let i = 0; i < numString.length; i++) {
     let digit = (fromBase != 64) ? DIGITORDER.indexOf(numString[i]) : DIGITS_SIXTY_FOUR.indexOf(numString[i]);
